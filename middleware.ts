921452b1ffc6666acefdf5ae8861be6a6257fa3b
unknown <ohseyeong041020@gmail.com>
2026-03-02 17:21:28 +0900

    dd

--- middleware.ts
+++ middleware.ts
@@ -39,8 +39,9 @@ export async function middleware(request: NextRequest) {
         // 대시보드로 보내줌
         return NextResponse.redirect(new URL("/admin/dashboard", request.url));
     }
-    /*
+
     // 3. 지원 기간 체크: /apply 경로는 기간 내에만 접근 가능
+    /*
     const APPLY_START = new Date("2026-03-03T00:00:00+09:00"); // 시작일
     const APPLY_END = new Date("2026-03-09T23:59:00+09:00"); // 마감일
     const now = new Date();
@@ -53,14 +54,14 @@ export async function middleware(request: NextRequest) {
         }
     }
 
-    return response;
-}*/
+    return response;*/
+}
 
-    export const config = {
-        matcher: [
-            "/admin/:path*",
-            "/login",
-            "/apply",
-            "/apply/((?!not-open|closed|about).+)", // not-open, closed, about 제외한 /apply 하위 경로
-        ],
-    };
+export const config = {
+    matcher: [
+        "/admin/:path*",
+        "/login",
+        "/apply",
+        "/apply/((?!not-open|closed|about).+)", // not-open, closed, about 제외한 /apply 하위 경로
+    ],
+};
